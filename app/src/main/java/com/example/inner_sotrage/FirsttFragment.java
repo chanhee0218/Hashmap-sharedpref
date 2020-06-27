057000abedd589653e1a9d74bf8abf1fb88b0500
chanhee0218 <5144eqq02@gmail.com>
2020-06-27 22:50:31 +0900

    4th commit

---
+++ app/src/main/java/com/example/inner_sotrage/FirsttFragment.java
@@ -0,0 +1,58 @@
+package com.example.inner_sotrage;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+class FirstFragment extends Fragment {
+    EditText editText;
+    Button button;
+    String editString;
+    //HashMap <String, String> hashMap;
+    Set<String> set= new HashSet<String>();
+    int key=0;
+    int value=0;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable final Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.activity_firstt_fragment,container,false);
+        Context context=getContext();
+        editText=view.findViewById(R.id.sharedEdit);
+        button=view.findViewById(R.id.sharedPushBtn);
+        final Intent intent=new Intent(context,MainActivity.class);
+        editString=editText.getText().toString();
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(editString.getBytes().length>0){
+                    SharedPreferences sharedPreferences = getActivity().getSharedPreferences("Note",Context.MODE_PRIVATE);
+                    set.add(editString);
+                    set=sharedPreferences.getStringSet(editString,null);
+                    List<String> list=new ArrayList<String>(set);
+                    Editor editor=sharedPreferences.edit();
+
+                    editor.putString(Integer.toString(key),editString);
+
+                }
+            }
+        });
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
--- app/src/main/java/com/example/inner_sotrage/MainActivity.java
+++ app/src/main/java/com/example/inner_sotrage/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.inner_sotrage;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -9,6 +12,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,69 +22,37 @@ import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
     BottomNavigationView bottomNavigationView;
-    EditText editText;
-    Button PushBtn;
-    TextView checkText;
-    private static final int key=0;
-    HashMap<Integer, String> hashMap;
-    String content;
-    int cnt=0;
+    private FragmentManager fragmentManager= getSupportFragmentManager();
+    private FirstFragment firstFragment=new FirstFragment();
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        editText = findViewById(R.id.editText);
-        content=editText.getText().toString();
-        PushBtn = findViewById(R.id.PushBtn);
-        checkText = findViewById(R.id.checkTxt);
+        bottomNavigationView=findViewById(R.id.bottom_navigation_view);
+        FragmentTransaction transaction=fragmentManager.beginTransaction();
+        transaction.replace(R.id.frameLayout,firstFragment).commitAllowingStateLoss();
 
 
-        bottomNavigationView = findViewById(R.id.bottom_navigation_view);
-        SharedPreferences sharedPreferences=getSharedPreferences("sFile",key);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.first: {
+                FragmentTransaction transaction=fragmentManager.beginTransaction();
+                switch (item.getItemId()){
 
-                        PushBtn.setOnClickListener(new View.OnClickListener() {
-                            @Override
-                            public void onClick(View v) {
-                                if(content.getBytes().length<=0){
-                                    Toast.makeText(MainActivity.this, "내용을 입력 해주세요", Toast.LENGTH_SHORT).show();
-                                }
-                                else{
-                                    Hash_map(content,key);
-                                    cnt++;
-                                }
-                            }
-                        });
+                    case R.id.first:{
+                         transaction.replace(R.id.frameLayout, firstFragment);
                     }
-                    case R.id.second: {
-                        Toast.makeText(MainActivity.this, "두번쨰 화면", Toast.LENGTH_SHORT).show();
-                        break;
+                    case R.id.second:{
+
                     }
-                    case R.id.third: {
-                        Toast.makeText(MainActivity.this, "세번쨰 화면", Toast.LENGTH_SHORT).show();
-                        break;
+                    case R.id.third:{
+
                     }
                 }
                 return false;
             }
         });
-
     }
-    public HashMap<Integer, String> Hash_map(String value, int hashcode){
-        hashMap=new HashMap<Integer,String>();
-        String receive=content;
-        int hash_code=cnt;
-        SharedPreferences sharedPreferences= getSharedPreferences("Sfile",MODE_PRIVATE);
-        sharedPreferences.edit().putString(receive, Integer.toString(hash_code)).apply();
-        hashMap.put(hash_code,receive);
-
-        return hashMap;
-    }
-
 }
+
 
